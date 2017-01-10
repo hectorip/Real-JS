bc69450b6878ab1edcbfd96274e6e4b2dd422542
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-01-10 00:01:15 -0600

    Explicación de comparación

--- comparaciones/main.js
+++ comparaciones/main.js
@@ -1,1 +1,8 @@
-// Acá va el JS
+
+
+// comparción estricta contra no estricta
+
+5 == "5" // convierte los dos en el mismo tipo y después compara
+
+5 === "5" // compara sin convertir ningún tipo
+
