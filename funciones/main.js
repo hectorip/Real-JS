21348564295af87e1980b5fb5b3ff45cbffe486d
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-01-11 23:48:29 -0600

    funciones como objetos

---
+++ funciones/main.js
@@ -0,0 +1,12 @@
+// Acá va el JS
+
+function greet(who){
+    console.log(who);
+}
+
+// Podemos aignarle propiedades a la función, lo cuál demuestra que es un objeto.
+
+greet.hello = "Hola";
+
+console.log(greet.hello); // "Hola"
+
