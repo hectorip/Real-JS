6b62310984aa120ee294c0708c376152d067227a
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-01-18 23:59:03 -0600

    Explicación de hoisting

--- variables/main.js
+++ variables/main.js
@@ -2,6 +2,10 @@
 
 
 function la_funcion(){
-    var la_variable = 0; // pertenece a la función
-    una_variable_global = 0; // pertenece al scope global
+    console.log(la_variable); // variable indefinida, con valor 'undefined'
+    var la_variable = 0; // pertenece a la función, se le aplica hoisting
+    una_variable_global = 0; // pertenece al scope global, mala idea
+    //console.log(una_variable_sin_hoisting) // error
+    let una_variable_sin_hoisting = 0;
 }
+
